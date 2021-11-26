d4c13e808899318ea9be8f0f3582687be412df09
David Tang <dtang859@gmail.com>
2021-11-25 20:38:05 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const source = new EventSource(
+      `${process.env.REACT_APP_API_ENDPOINT}/count`
+    );
+
+    source.addEventListener("open", () => {
+      console.log("EventSource connection opened");
+    });
+
+    source.addEventListener("message", (message) => {
+      setCount(message.data);
+    });
+
+    source.addEventListener("error", (error) => {
+      console.error("Error: ", error);
+    });
+
+    return () => {
+      source.close();
+    };
+  }, []);
+
+  return <div>{count}</div>;
 }
-
-export default App;
